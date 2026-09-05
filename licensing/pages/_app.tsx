97308321d35d15027968ff37ad57dd3449167b84
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2026-09-04 20:13:46 -0700

    Add first-party web analytics

--- licensing/pages/_app.tsx
+++ licensing/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/next';
 import { IBM_Plex_Mono, Manrope } from 'next/font/google';
 import '../styles/globals.css';
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${sans.variable} ${mono.variable}`}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
